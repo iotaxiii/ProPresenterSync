694d7795b141ab54558fbd86b2bc1c75356402b6
Jason Machata <iotaxiii@gmail.com>
2019-05-03 20:33:08 -0500

    initial https server changes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const WebSocket = require('ws');
 const config = require('config');
+const fs = require('fs');
+const https = require('https');
+const remote = config('ws.remote');
 
-let ws = new WebSocket.Server({ port: config.remote.port });
+const server = https.createServer({
+   cert: fs.readFileSync(remote.certPath),
+   key: fs.readFileSync(remote.keyPath),
+   port: remote.port
+});
+
+let ws = new WebSocket.Server({ server });
 
 function authenticateConnection(req) {
-    //TODO
+
 }
 
 ws.on('connection', function (socket, req) {
